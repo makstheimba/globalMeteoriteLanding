cadbf666d37af20273579d22d19c772ed2bf772e
makstheimba <makstheimba@yandex.ru>
2017-11-19 14:43:07 +0300

    basic globe with drag

---
+++ index.js
@@ -0,0 +1,62 @@
+require('d3-selection-multi');
+
+const d3 = require('d3');
+const versor = require('./versor');
+const topojson = require('topojson');
+
+window.onload = () => {
+  const url =
+    'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
+  const canvas = d3.select('canvas');
+  const width = canvas.property('width');
+  const height = canvas.property('height');
+  const context = canvas.node().getContext('2d');
+
+  const projection = d3.geoOrthographic()
+    .scale((height - 10) / 2)
+    .translate([width / 2, height / 2])
+    .precision(0.1);
+
+  const path = d3.geoPath().projection(projection).context(context);
+
+  let render = function () {};
+  let v0; // Mouse position in Cartesian coordinates at start of drag gesture.
+  let r0; // Projection rotation as Euler angles at start.
+  let q0; // Projection rotation as versor at start.
+
+  function dragstarted() {
+    v0 = versor.cartesian(projection.invert(d3.mouse(this)));
+    r0 = projection.rotate();
+    q0 = versor(r0);
+  }
+
+  function dragged() {
+    const v1 = versor.cartesian(projection.rotate(r0).invert(d3.mouse(this)));
+    const q1 = versor.multiply(q0, versor.delta(v0, v1));
+    const r1 = versor.rotation(q1);
+
+    projection.rotate(r1);
+    render();
+  }
+
+  canvas.call(d3.drag().on('start', dragstarted).on('drag', dragged));
+
+  d3.json('https://unpkg.com/world-atlas@1/world/110m.json', (error, world) => {
+    if (error) throw error;
+
+    const sphere = { type: 'Sphere' };
+    const land = topojson.feature(world, world.objects.land);
+
+    render = function () {
+      context.clearRect(0, 0, width, height);
+      context.beginPath(); path(sphere); context.fillStyle = '#fff'; context.fill();
+      context.beginPath(); path(land); context.fillStyle = '#000'; context.fill();
+      context.beginPath(); path(sphere); context.stroke();
+    };
+
+    render();
+  });
+  d3.json(url, (error, meteors) => {
+    console.log(meteors);
+  });
+};
---
+++ versor.js
@@ -0,0 +1,73 @@
+const cross = (v0, v1) => [
+  (v0[1] * v1[2]) - (v0[2] * v1[1]),
+  (v0[2] * v1[0]) - (v0[0] * v1[2]),
+  (v0[0] * v1[1]) - (v0[1] * v1[0]),
+];
+
+const dot = (v0, v1) => (v0[0] * v1[0]) + (v0[1] * v1[1]) + (v0[2] * v1[2]);
+
+const factory = () => {
+  const { acos, asin, atan2, cos, max, min, PI, sin, sqrt } = Math;
+  const radians = PI / 180;
+  const degrees = 180 / PI;
+
+  // Returns the unit quaternion for the given Euler rotation angles [λ, φ, γ].
+  function versor(e) {
+    const l = (e[0] / 2) * radians;
+    const sl = sin(l);
+    const cl = cos(l); // λ / 2
+    const p = (e[1] / 2) * radians;
+    const sp = sin(p);
+    const cp = cos(p); // φ / 2
+    const g = (e[2] / 2) * radians;
+    const sg = sin(g);
+    const cg = cos(g); // γ / 2
+
+    return [
+      (cl * cp * cg) + (sl * sp * sg),
+      (sl * cp * cg) - (cl * sp * sg),
+      (cl * sp * cg) + (sl * cp * sg),
+      (cl * cp * sg) - (sl * sp * cg),
+    ];
+  }
+
+  // Returns Cartesian coordinates [x, y, z] given spherical coordinates [λ, φ].
+  versor.cartesian = (e) => {
+    const l = e[0] * radians;
+    const p = e[1] * radians;
+    const cp = cos(p);
+
+    return [cp * cos(l), cp * sin(l), sin(p)];
+  };
+
+  // Returns the Euler rotation angles [λ, φ, γ] for the given quaternion.
+  versor.rotation = q => [
+    atan2(2 * ((q[0] * q[1]) + (q[2] * q[3])), 1 - (2 * ((q[1] * q[1]) + (q[2] * q[2])))) * degrees,
+    asin(max(-1, min(1, 2 * ((q[0] * q[2]) - (q[3] * q[1]))))) * degrees,
+    atan2(2 * ((q[0] * q[3]) + (q[1] * q[2])), 1 - (2 * ((q[2] * q[2]) + (q[3] * q[3])))) * degrees,
+  ];
+
+  // Returns the quaternion to rotate between two cartesian points on the sphere.
+  versor.delta = (v0, v1) => {
+    const w = cross(v0, v1);
+    const l = sqrt(dot(w, w));
+    if (!l) {
+      return [1, 0, 0, 0];
+    }
+    const t = acos(max(-1, min(1, dot(v0, v1)))) / 2;
+    const s = sin(t); // t = θ / 2
+    return [cos(t), (w[2] / l) * s, (-w[1] / l) * s, (w[0] / l) * s];
+  };
+
+  // Returns the quaternion that represents q0 * q1.
+  versor.multiply = (q0, q1) => [
+    ((q0[0] * q1[0]) - (q0[1] * q1[1])) - ((q0[2] * q1[2]) - (q0[3] * q1[3])),
+    ((q0[0] * q1[1]) + (q0[1] * q1[0])) + ((q0[2] * q1[3]) - (q0[3] * q1[2])),
+    ((q0[0] * q1[2]) - (q0[1] * q1[3])) + ((q0[2] * q1[0]) + (q0[3] * q1[1])),
+    ((q0[0] * q1[3]) + (q0[1] * q1[2])) - ((q0[2] * q1[1]) + (q0[3] * q1[0])),
+  ];
+
+  return versor;
+};
+
+module.exports = factory();
