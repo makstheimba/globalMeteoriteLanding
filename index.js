1e013deb7e4421ee4ecd2f00d7d4830a95036202
makstheimba <makstheimba@yandex.ru>
2018-01-09 09:36:59 +0300

    Add tooltip for meteors

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const d3 = require('d3');
 const versor = require('./versor');
 const topojson = require('topojson');
 
+const getTooltipHTML = ({ name, mass, year }) => (
+  `<strong>${name}</strong><br />
+  ${mass} g<br />
+  Fell in ${new Date(year).getFullYear()}
+  `
+);
+
 window.onload = () => {
-  const canvas = d3.select('canvas');
+  const canvas = d3.select('#mainCanvas');
   const width = canvas.property('width');
   const height = canvas.property('height');
   const context = canvas.node().getContext('2d');
+  const tooltip = d3.select('.tooltip');
 
   const projection = d3.geoOrthographic()
     .scale((height - 10) / 2)
@@ -17,10 +25,16 @@ window.onload = () => {
 
   const path = d3.geoPath().projection(projection).context(context);
 
+  const hiddenCanvas = d3.select('#hiddenCanvas');
+  const hiddenContext = hiddenCanvas.node().getContext('2d');
+  const hiddenProjection = d3.geoEquirectangular();
+  const hiddenPath = d3.geoPath().projection(hiddenProjection).context(hiddenContext);
+
   let render = () => {};
   let v0; // Mouse position in Cartesian coordinates at start of drag gesture.
   let r0; // Projection rotation as Euler angles at start.
   let q0; // Projection rotation as versor at start.
+  let selectedMeteor = null;
 
   function dragstarted() {
     v0 = versor.cartesian(projection.invert(d3.mouse(this)));
@@ -42,15 +56,27 @@ window.onload = () => {
   d3.queue()
     .defer(d3.json, 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json')
     .defer(d3.json, 'https://unpkg.com/world-atlas@1/world/110m.json')
-    .await((error, meteors, world) => {
+    .await((error, dirtyMeteors, world) => {
       if (error) throw error;
-      const meteorsMass = meteors.features.map(meteor => +meteor.properties.mass).filter(m => m);
+      const meteorsMass = dirtyMeteors.features.map(meteor => +meteor.properties.mass).filter(m => m);
       const land = topojson.feature(world, world.objects.land);
-      const circle = d3.geoCircle();
+      const circle = d3.geoCircle().precision(25);
       const rScale = d3.scaleSqrt()
         .clamp(true)
         .domain([Math.min(...meteorsMass), Math.max(...meteorsMass)])
         .range([0.1, 5]);
+      const meteors = dirtyMeteors.features
+        .filter(meteor => meteor.geometry)
+        .map(meteor => Object.assign({ radius: rScale(meteor.properties.mass) }, meteor));
+
+      meteors.forEach((meteor, i) => {
+        const iStr = `${i}`.padStart(3, 0);
+
+        hiddenContext.beginPath();
+        hiddenPath(circle.center(meteor.geometry.coordinates).radius(meteor.radius)());
+        hiddenContext.fillStyle = `rgb(${(iStr[0] * 10) + 5}, ${(iStr[1] * 10) + 5}, ${(iStr[2] * 10) + 5})`;
+        hiddenContext.fill();
+      });
 
       render = () => {
         context.clearRect(0, 0, width, height);
@@ -62,19 +88,52 @@ window.onload = () => {
 
         context.beginPath();
         path(land);
-        context.fillStyle = '#000';
+        context.fillStyle = '#555';
         context.fill();
 
-        meteors.features.forEach((meteor) => {
-          if (meteor.geometry) {
-            context.beginPath();
-            path(circle.center(meteor.geometry.coordinates).radius(rScale(meteor.properties.mass))());
-            context.fillStyle = '#911';
-            context.fill();
-          }
+        meteors.forEach((meteor) => {
+          context.beginPath();
+          path(circle.center(meteor.geometry.coordinates).radius(meteor.radius)());
+          context.fillStyle = '#911';
+          context.fill();
         });
       };
 
       render();
+
+      canvas.on('mousemove', function () {
+        const pos = d3.mouse(this);
+        const latlong = projection.invert(pos);
+        const hiddenPos = hiddenProjection(latlong);
+        const p = hiddenPos[0] > -1
+          ? hiddenContext.getImageData(hiddenPos[0], hiddenPos[1], 1, 1).data
+          : null;
+        const drawMeteor = (meteor, isSelected) => {
+          context.beginPath();
+          path(circle.center(meteor.geometry.coordinates).radius(meteor.radius)());
+          context.fillStyle = isSelected ? '#0ad' : '#911';
+          context.fill();
+        };
+
+        if (p !== null && p[0] !== 0) {
+          const index = (Math.floor(p[0] / 10) * 100) + (Math.floor(p[1] / 10) * 10) + (Math.floor(p[2] / 10));
+          const meteor = meteors[index];
+
+          if (index !== selectedMeteor) {
+            if (selectedMeteor !== null) {
+              drawMeteor(meteors[selectedMeteor], false);
+            }
+            selectedMeteor = index;
+            drawMeteor(meteor, true);
+            tooltip.classed('tooltip-hidden', false)
+              .html(getTooltipHTML(meteor.properties))
+              .styles({ left: `${pos[0]}px`, top: `${pos[1]}px` });
+          }
+        } else {
+          drawMeteor(meteors[selectedMeteor], false);
+          selectedMeteor = null;
+          tooltip.classed('tooltip-hidden', true);
+        }
+      });
     });
 };
